d22cb2d874cf669f52af62245ff39b752faeae94
bashantad <dahal_bsd@yahoo.com>
2014-08-24 12:47:54 +1000

    populate_teacher.sql added

---
+++ SQL/populate_teacher.sql
@@ -0,0 +1,19 @@
+INSERT INTO  `wordpress`.`wp_users` (
+`ID` ,
+`user_login` ,
+`user_pass` ,
+`user_nicename` ,
+`user_email` ,
+`user_url` ,
+`user_registered` ,
+`user_activation_key` ,
+`user_status` ,
+`display_name` ,
+`remember_token` ,
+`updated_at` ,
+`is_admin` ,
+`role`
+)
+VALUES (
+NULL ,  'firebird_teacher',  '$P$B.uD3VLLyoE5uyTPUCNK7M0u89mFYE.',  'firebird_teacher',  'firebird@teacher.latrobe.edu.au',  '', '2014-08-23 04:26:55',  '',  '0',  'Firebird Teacher',  'D9GpQsUFOUUN0no9Up9p',  '2014-08-24',  'N',  'teacher'
+);
